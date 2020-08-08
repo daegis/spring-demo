ce50c7ed937d71012e934c6ad78dc3bce769fe9d
缐英达 <xianyingda@guazi.com>
2020-08-08 17:59:03 +0800

    测试java spi多实现

---
+++ src/main/java/cn/ihna/demo/spring/service/impl/HeadMasterService.java
@@ -0,0 +1,15 @@
+package cn.ihna.demo.spring.service.impl;
+
+import cn.ihna.demo.spring.service.SchoolService;
+
+/**
+ * @author xianyingda@gmail.com
+ * @serial
+ * @since 2020/8/7 1:06 下午
+ */
+public class HeadMasterService implements SchoolService {
+    @Override
+    public void doSpeech() {
+        System.out.println("I am a head master");
+    }
+}
